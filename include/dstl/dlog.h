ff969c604290dcd5e7741b81da1cb64e7a26cd25
Jon Silverberg <jpsilverberg@gmail.com>
2025-11-09 13:55:21 -0600

    Initial dg0

--- include/dstl/dlog.h
+++ include/dstl/dlog.h
@@ -1,6 +1,6 @@
 #pragma once
 
 // DSTL Logging Library
-// This is the main header for the DSTL logging functionality
+// This header provides access to the DSTL logging functionality
 
 #include "../../DataLog/DLog.h"
