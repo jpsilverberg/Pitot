da908f026092e3cfe59a4f9d99f6957a71674949
Jon Silverberg <jpsilverberg@gmail.com>
2025-11-14 09:07:08 -0600

    Update submodule commits for LinAlg, Numbers, and Quadrature; add dtests.h header for logging functionality

---
+++ include/dstl/dtests.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// DSTL Logging Library
+// This header provides access to the DSTL logging functionality
+
+#include <gtest/gtest.h>
+#include <thread>
+
+#include <dstl/dlog.h>
+#define ISNEAR(a, b) \
+VAR(a);               \
+VAR(b);               \
+ASSERT_NEAR(static_cast<double>(a), static_cast<double>(b), 1.e-6)
+
