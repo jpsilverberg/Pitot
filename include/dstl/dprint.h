2dac94df87803b6b9c4423a9de1c74bb1442f64e
Jon Silverberg <jpsilverberg@gmail.com>
2026-01-01 10:35:41 -0600

    added scripts and change dprint

--- include/dstl/dprint.h
+++ include/dstl/dprint.h
@@ -3,4 +3,4 @@
 // DSTL Logging Library
 // This is the main header for the DSTL logging functionality
 
-#include "../../Logger/include/dstl/dprint.h"
+#include "../../StringUtils/include/dstl/dprint.h"
---
+++ scripts/templates/component_base/include/dstl/__COMPONENT_NAME__.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace dstl
+{
+namespace __COMPONENT_NAME__
+{
+}
+} // namespace dstl
